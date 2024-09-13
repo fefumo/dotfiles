67c93eb12ed6d8b40fcbf3977088229ec2dae5f4
fedos <fedrodj@gmail.com>
2024-09-13 21:51:13 +0300

    add initial cfgs

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf ~/.config/i3 ~/dotfiles/i3_config
+ln -sf ~/.config/alacritty ~/dotfiles/alacritty_config
+ln -sf ~/.config/polybar ~/dotfiles/polybar_config
+ln -sf ~/.config/rofi ~/dotfiles/rofi_config
+ln -sf ~/.config/picom ~/dotfiles/picom_config
